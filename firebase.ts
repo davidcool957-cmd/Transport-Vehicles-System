034b8099d3fbf8e46b323be5dd0253cf01913d1f
davidcool957-cmd <davidcool957@gmail.com>
2026-02-05 16:12:25 +0300

    refactor: Remove Firebase and update dependencies
    The Firebase SDK has been removed as it is no longer required for authentication. Dependency versions have been updated, and import maps in index.html adjusted accordingly. Basic styling for the body and root element has also been added.

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,2 @@
-
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBG_7IMuFpIQth16cz98CIXHdSX24-GgLc",
-  authDomain: "oiltankers-2e62b.firebaseapp.com",
-  projectId: "oiltankers-2e62b",
-  storageBucket: "oiltankers-2e62b.firebasestorage.app",
-  messagingSenderId: "638767138928",
-  appId: "1:638767138928:web:9bc105d44b750e96944cb5"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+// This file has been deprecated in favor of Supabase.
+export {};
